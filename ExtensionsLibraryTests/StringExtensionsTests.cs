a3167a2f79ba7183dc72da10a4d0cc018698fec6
takase <y.takase331@gmail.com>
2021-11-25 20:48:12 +0900

    プロジェクト ファイルを追加します。

---
+++ ExtensionsLibraryTests/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsLibrary.Tests
+{
+    [TestClass()]
+    public class StringExtensionsTests
+    {
+        [TestMethod()]
+        public void IsNullOrEmptyTest()
+        {
+            var value = "";
+            Assert.IsTrue(value.IsNullOrEmpty());
+
+            value = "value";
+            Assert.IsFalse(value.IsNullOrEmpty());
+        }
+    }
+}
