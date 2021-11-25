b0961a65e7055123c406e95b5b6b7dffa78d2e77
takase <y.takase331@gmail.com>
2021-11-25 23:48:57 +0900

    ContainsIndex

---
+++ ExtensionsLibrary/IListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsLibrary
+{
+    public static class IListExtensions
+    {
+        public static bool ContainsIndex<T>(this IList<T> self, int index)
+        {
+            return index < self.Count;
+        }
+    }
+}
---
+++ ExtensionsLibraryTests/IListExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsLibrary.Tests
+{
+    [TestClass()]
+    public class IListExtensionsTests
+    {
+        [TestMethod()]
+        public void ContainsIndexTest()
+        {
+            var array = new int[] { 1, 1 };
+
+            Assert.IsTrue(array.ContainsIndex(0));
+            Assert.IsTrue(array.ContainsIndex(1));
+            Assert.IsFalse(array.ContainsIndex(2));
+        }
+    }
+}
